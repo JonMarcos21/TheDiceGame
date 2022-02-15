6edff368b0e74d525b83d8c3363fed376f911ed9
= <=>
2022-02-15 19:35:33 +0100

    firebase implementada

--- app/src/main/java/com/example/thedicegame/Formulario.java
+++ app/src/main/java/com/example/thedicegame/Formulario.java
@@ -1,14 +1,45 @@
 package com.example.thedicegame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Formulario extends AppCompatActivity {
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.formulario);
+
+
+
+
     }
+
+    //metodo de regitrar usuario
+
+
+
 }
