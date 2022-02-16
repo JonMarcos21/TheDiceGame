6db2043821e3d05bc86fffdc8af98ff35cfa9740
= <=>
2022-02-16 21:15:11 +0100

    cerrar sesion

--- app/src/main/java/com/example/thedicegame/Inicio.java
+++ app/src/main/java/com/example/thedicegame/Inicio.java
@@ -2,13 +2,33 @@ package com.example.thedicegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Inicio extends AppCompatActivity {
 
+    private Button salir;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        mAuth = FirebaseAuth.getInstance();
+        salir = (Button) findViewById(R.id.salir);
+
+        salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mAuth.signOut();
+                startActivity(new Intent(Inicio.this,Registro.class));
+                finish();
+            }
+        });
     }
 }
