d6a916128da71966883132dd4d4bd79a6a1a7ef6
= <=>
2022-02-18 18:08:35 +0100

    interfaz cambiada

--- app/src/main/java/com/example/thedicegame/Registro.java
+++ app/src/main/java/com/example/thedicegame/Registro.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,8 +18,10 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Registro extends AppCompatActivity {
 
+    // creacion de variables para añadir datos
     Button formulario;
     Button inicio;
+    Button play;
 
     private EditText contraseña1;
     private EditText correo1;
@@ -29,17 +32,23 @@ public class Registro extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
 
+    //conecto las variables con el xml
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        formulario=(Button) findViewById(R.id.registrarse);
+
         inicio =(Button) findViewById(R.id.acceso);
 
         contraseña1 = (EditText) findViewById(R.id.Contraseña);
         correo1 = (EditText) findViewById(R.id.Usuario);
         mAuth=FirebaseAuth.getInstance();
+
+        play = (Button)findViewById(R.id.btnPlay);
+        play.setEnabled(false);
+        play.setTextColor(Color.parseColor("#9E9E9E"));
+
 
       /*  inicio.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +68,7 @@ public class Registro extends AppCompatActivity {
 
     }
 
+    //creo un metodo para el boton de login que en caso de que esten los campos bien pasara de pantalla
     public void acceso(View view) {
 
         textUsuario = correo1.getText().toString();
@@ -72,6 +82,7 @@ public class Registro extends AppCompatActivity {
             Toast.makeText(Registro.this, "Completa los campos", Toast.LENGTH_SHORT).show();
         }
     }
+    //metodo para pasar al formulario
     public void registro(View view){
 
         Intent registro = new Intent(this, Formulario.class);
@@ -79,13 +90,23 @@ public class Registro extends AppCompatActivity {
         finish();
 
     }
+    public void play(View view){
+
+        Intent play = new Intent(this, Juego.class);
+        startActivity(play);
+        finish();
+
+    }
+    //metodo para comprobar que se añaden los datos
     private void loginuser(){
 
         mAuth.signInWithEmailAndPassword(textUsuario, textContraseña).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()){
-                    startActivity(new Intent(Registro.this , Inicio.class));
+                    play.setEnabled(true);
+                    play.setTextColor(Color.parseColor("#ffffff"));
+
 
                 }
                 else {
