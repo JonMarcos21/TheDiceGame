6fb021fcdfc11f3de71327d5bda4d1d56c93fbe3
= <=>
2022-02-26 19:24:54 +0100

    app casi acabada

--- app/src/main/java/com/example/thedicegame/Puntuacion.java
+++ app/src/main/java/com/example/thedicegame/Puntuacion.java
@@ -2,13 +2,38 @@ package com.example.thedicegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Puntuacion extends AppCompatActivity {
+
+    Button jugardenuevo;
+    Button salir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.puntuacion);
+
+        jugardenuevo = (Button) findViewById(R.id.jugarotraves);
+        salir = (Button) findViewById(R.id.salirrr);
     }
+
+    public void play(View view){
+
+        Intent play = new Intent(this, Juego.class);
+        startActivity(play);
+        finish();
+
+    }
+    public void salir(View view){
+
+        Intent salir = new Intent(this, Registro.class);
+        startActivity(salir);
+        finish();
+
+    }
+
 }
